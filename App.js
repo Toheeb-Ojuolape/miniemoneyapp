a303a7da5aa485e99fb2f1263c76a96583720abf
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-01-02 19:08:17 +0100

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
-import { BackHandler, AppState, ActivityIndicator, Alert } from 'react-native';
-import { WebView } from 'react-native-webview';
-
+import React, { Component } from "react";
+import {
+  BackHandler,
+  AppState,
+  ActivityIndicator,
+  StatusBar,
+  Alert,
+} from "react-native";
+import { WebView } from "react-native-webview";
 
 
 export default class tellbooks extends Component {
@@ -11,72 +16,86 @@ export default class tellbooks extends Component {
   }
 
   state = {
-    appState: AppState.currentState
+    appState: AppState.currentState,
   };
 
   componentDidMount() {
-    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
     AppState.addEventListener("change", this._handleAppStateChange);
   }
 
   componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
     AppState.removeEventListener("change", this._handleAppStateChange);
   }
 
   handleBackButton = () => {
     this.WEBVIEW_REF.current.goBack();
     return true;
-  }
+  };
 
   onNavigationStateChange(navState) {
     this.setState({
-      canGoBack: navState.canGoBack
+      canGoBack: navState.canGoBack,
     });
   }
 
-  _handleAppStateChange = nextAppState => {
-
+  _handleAppStateChange = (nextAppState) => {
     if (
       this.state.appState.match(/inactive|background/) &&
       nextAppState === "active"
     ) {
-
     }
     this.setState({ appState: nextAppState });
   };
 
-
   render() {
     return (
       <>
+        <StatusBar
+          animated={true}
+          backgroundColor="#ffffff"
+          barStyle={"dark-content"}
+        />
         <WebView
           source={{ uri: "https://app.miniemoney.com/app" }}
           ref={this.WEBVIEW_REF}
           onNavigationStateChange={this.onNavigationStateChange.bind(this)}
           startInLoadingState={true}
-          renderLoading={() => <ActivityIndicator size="large" color="#0066f5" style={{ position: 'absolute', left: 0, right: 0, bottom: 0, top: 0, }} />}
-          style={{ marginTop: 40 }}
+          
+          renderLoading={() => (
+            <ActivityIndicator
+              size="large"
+              color="#0066f5"
+              style={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                bottom: 0,
+                top: 0,
+              }}
+            />
+          )}
+          style={{ marginTop: 0 }}
           pullToRefreshEnabled={true}
           onContentProcessDidTerminate={(syntheticEvent) => {
             const { nativeEvent } = syntheticEvent;
-            console.warn('Content process terminated, reloading', nativeEvent);
-            this.WEBVIEW_REF.current.reload()
-            Alert.alert('App went offline. Reloading...')
-            this.WEBVIEW_REF.current.reload()
+            console.warn("Content process terminated, reloading", nativeEvent);
+            this.WEBVIEW_REF.current.reload();
+            Alert.alert("App went offline. Reloading...");
+            this.WEBVIEW_REF.current.reload();
           }}
-          onRenderProcessGone={syntheticEvent => {
+          onRenderProcessGone={(syntheticEvent) => {
             const { nativeEvent } = syntheticEvent;
-            console.warn(
-              'WebView Crashed: ',
-              nativeEvent.didCrash,
-            );
-            this.WEBVIEW_REF.current.reload()
-            window.alert('App went offline. Reloading...')
-            this.WEBVIEW_REF.current.reload()
+            console.warn("WebView Crashed: ", nativeEvent.didCrash);
+            this.WEBVIEW_REF.current.reload();
+            window.alert("App went offline. Reloading...");
+            this.WEBVIEW_REF.current.reload();
           }}
         />
+
       </>
     );
   }
 }
+
